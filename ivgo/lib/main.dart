406ba010bb36dea32198e26c74866279e0bb2eca
MegaByteMark <mark.hart89@gmail.com>
2025-02-23 22:43:33 +0000

    Add TODOs for future features and enhancements in StopwatchListPage

--- ivgo/lib/main.dart
+++ ivgo/lib/main.dart
@@ -25,3 +25,20 @@ class IVGoApp extends StatelessWidget {
     );
   }
 }
+
+//TODO - Add a list of StopwatchTimers to the StopwatchListPage
+//TODO - Add a way to add new StopwatchTimers to the list
+//TODO - Add a way to remove StopwatchTimers from the list
+//TODO - Add a way to start, stop, and reset each StopwatchTimer
+//TODO - Add a way to reorder the StopwatchTimers in the list
+//TODO - Add Cupertino support for iOS devices
+//TODO - Add a way to save the list of StopwatchTimers to local storage
+//TODO - Add a way to load the list of StopwatchTimers from local storage
+//TODO - Add a way to clear the list of StopwatchTimers from local storage
+//TODO - Add a way to share the list of StopwatchTimers with others
+//TODO - Add a way to import a list of StopwatchTimers from others
+//TODO - Add a way to export a list of StopwatchTimers to others
+//TODO - Add a way to change the theme of the app (light/dark mode)
+//TODO - Add a way to change the language of the app
+//TODO - Add adverts and sponsorship to the app
+//TODO - Add analytics and crash reporting to the app
